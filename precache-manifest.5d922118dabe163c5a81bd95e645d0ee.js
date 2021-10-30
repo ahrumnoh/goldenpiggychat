d7b36bffb88cf2f641a393a826ed6390bb4c0ead
ahrum noh <sydneyroseenglish@gmail.com>
2021-10-30 15:15:01 +1100

    Updates

---
+++ precache-manifest.5d922118dabe163c5a81bd95e645d0ee.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39d13d731f8455f542d6925087a2b1f2",
+    "url": "/goldenpiggychat/index.html"
+  },
+  {
+    "revision": "ec2f2a67d2b150801799",
+    "url": "/goldenpiggychat/static/css/main.459d4e05.chunk.css"
+  },
+  {
+    "revision": "65382544370b188ca4ea",
+    "url": "/goldenpiggychat/static/js/2.39ce38bc.chunk.js"
+  },
+  {
+    "revision": "0885da1b7640f883c678fe2c0368c3a4",
+    "url": "/goldenpiggychat/static/js/2.39ce38bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec2f2a67d2b150801799",
+    "url": "/goldenpiggychat/static/js/main.5dd8a4e6.chunk.js"
+  },
+  {
+    "revision": "1650b034bfbca77083ff",
+    "url": "/goldenpiggychat/static/js/runtime-main.9b880746.js"
+  },
+  {
+    "revision": "abbd2e0ec6111819bada24f4f113fcde",
+    "url": "/goldenpiggychat/static/media/executium-4KJJezDyo3M-unsplash.abbd2e0e.ico"
+  }
+]);
